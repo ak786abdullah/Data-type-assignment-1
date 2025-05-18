74a0843fa79394178b96c2833d32c5e0330609e6
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-05-18 16:25:03 +0500

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,39 @@
+#float
+#dacimle number 
+weight =50.5 #in kg 
+height= 5.11 #in feet  
+print(weight,height)
+print(weight)
+print(height)
+print(weight+height)
+#string
+#sequence of text ,charactors 
+#value of variable ( string value) always in ""
+firstname ="abdullah"
+secondname="khan"
+print(firstname,secondname)
+#out put will be at one line each value separated by space 
+print(firstname)
+print(secondname)
+#every output at new lane 
+print(firstname+secondname)
+#output will be horizantal with out space 
+#is it possible we print all data type togather?
+print("10kg weight 5.11ft height ")
+#we can print all data type togather when we write value  in "" (string form ) 
+#integer =10 float=5.11 string=weight,height  
+#boolean 
+#condition true or false 
+fan_on=True
+fan_off=False 
+print(fan_on)
+print(fan_off)
+#useing all data type in one program
+#intergers
+quantity=3 #in litter 
+#float
+price_per_litter =210.76
+#string 
+item_name='petrol'
+total_price=quantity*price_per_litter
+print('you bought',quantity,'litter of',item_name,'for a total of',total_price,'dollars')
